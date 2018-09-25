4401c25ed03e85147291c35baa0987dbb5b497a7
羽岡理沙 <yp.r.haoka@gmail.com>
2018-09-25 14:38:46 +0900

    ScriptTestプロジェクト　Lesson4課題
    課題を作成しました。Bossの魔法攻撃はできませんでした。

---
+++ Assets/Test.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// 配列を初期化する
+		int[] array = {1, 23, 50, 27, 34};
+
+		// 配列の要素数のぶんだけ処理を繰り返す
+		for (int i = 0; i < array.Length; i++) {
+			// 順番に表示
+			Debug.Log (array [i]);
+		}
+		for (int i = array.Length - 1; i >= 0; i--) {
+			// 逆順に表示
+			Debug.Log (array [i]);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
+
